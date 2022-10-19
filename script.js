5c9c273d6cd7d0e3645c8f73fe051c4e513d3764
Alan Uraz <38016621+aloonballoon@users.noreply.github.com>
2022-10-19 03:59:53 +0000

    added JS to calculate age

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// task 1, 2, & 3
+let inputName = document.querySelector(".name");
+let inputYear = document.querySelector(".year");
+let message = document.querySelector(".message");
+let ageButton = document.querySelector(".age-button")
 
+// task 4
+let currentYear = 2022;
+
+// task 5 & 6
+let userName;
+let userYear;
+
+// task 7
+let age;
+
+
+
+ageButton.onclick = function() {
+  userName = inputName.value;
+  userYear = Number(inputYear.value);
+  let currentYear = new Date().getFullYear();  // returns the current year
+  age = currentYear - userYear;
+  message.innerHTML = `WOW, ${userName}, GETTING UP THERE! Almost ${age + 1}....`
+}
